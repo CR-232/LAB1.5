fd8ff4bdc0924cec83715ecf532263d3275af27d
OverMan21 <vladimirrusanovschi@gmail.com>
2025-10-30 14:05:55 +0200

    lab2 vladimir..

--- seminar/seminar.java
+++ seminar/seminar.java
@@ -3,33 +3,33 @@ package seminar;
 class seminar {
     public static void main(String[] args) {
 
-        //main
+        // Grupul principal (implicit)
         ThreadGroup mainGroup = Thread.currentThread().getThreadGroup();
 
-        //Creăm  G1 și G2
+        // Creăm grupurile G1 și G2
         ThreadGroup G1 = new ThreadGroup(mainGroup, "G1");
         ThreadGroup G2 = new ThreadGroup(mainGroup, "G2");
 
-        //G3 cu G1
+        // G3 este subgrup al G1
         ThreadGroup G3 = new ThreadGroup(G1, "G3");
 
-        //Grupul G3 cu Tha(3), Thb(3), Thc(3), Thd(3)
+        // Grupul G3 cu firele Tha(3), Thb(3), Thc(3), Thd(3)
         Thread Tha = new Fir("Tha", G3, 3);
         Thread Thb = new Fir("Thb", G3, 3);
         Thread Thc = new Fir("Thc", G3, 3);
         Thread Thd = new Fir("Thd", G3, 3);
 
-        //Grupul G2 cu Th1(4), Th2(5), Th3(5)
-        Thread Th1_G2 = new Fir("Th1", G2, 4);
-        Thread Th2_G2 = new Fir("Th2", G2, 5);
-        Thread Th3_G2 = new Fir("Th3", G2, 5);
+        //Grupul G2 cu firele Th1(4), Th2(5), Th3(5)
+        Thread Th1_G2 = new Fir("Th1_G2", G2, 4);
+        Thread Th2_G2 = new Fir("Th2_G2", G2, 5);
+        Thread Th3_G2 = new Fir("Th3_G2", G2, 5);
 
-        // Grupul  Main cu Th1(7), Th2(7), ThA(3)
-        Thread Th1_Main = new Fir("Th1", mainGroup, 7);
-        Thread Th2_Main = new Fir("Th2", mainGroup, 7);
-        Thread ThA_Main = new Fir("ThA", mainGroup, 3);
+        // Grupul principal cu Th1(7), Th2(7), ThA(3)
+        Thread Th1_Main = new Fir("Th1_Main", mainGroup, 7);
+        Thread Th2_Main = new Fir("Th2_Main", mainGroup, 7);
+        Thread ThA_Main = new Fir("ThA_Main", mainGroup, 3);
 
-        //Pornim
+        // Pornim toate firele
         Tha.start();
         Thb.start();
         Thc.start();
@@ -43,9 +43,33 @@ class seminar {
         Th2_Main.start();
         ThA_Main.start();
 
+        // Afișăm structura completă
+        System.out.println("\n==========================================");
+        System.out.println("=== STRUCTURA COMPLETĂ A FIRELOR ===");
+        System.out.println("==========================================\n");
+        afisareStructura(mainGroup, ""); // apelăm metoda recursivă
+    }
 
-        System.out.println("\n=== Structura completă a firelor și grupurilor ===");
-        mainGroup.list();
+    // Metodă recursivă pentru afișarea ierarhiei de grupuri și fire
+    public static void afisareStructura(ThreadGroup group, String indent) {
+        System.out.println(indent + "Grup: " + group.getName() + " (Prioritate maximă: " + group.getMaxPriority() + ")");
+        int activeThreads = group.activeCount();
+        Thread[] threads = new Thread[activeThreads];
+        group.enumerate(threads, false);
+
+        for (Thread t : threads) {
+            if (t != null)
+                System.out.println(indent + "   └─  Fir: " + t.getName() + " (Prioritate: " + t.getPriority() + ")");
+        }
+
+        int activeGroups = group.activeGroupCount();
+        ThreadGroup[] subGroups = new ThreadGroup[activeGroups];
+        group.enumerate(subGroups, false);
+
+        for (ThreadGroup g : subGroups) {
+            if (g != null)
+                afisareStructura(g, indent + "   ");
+        }
     }
 }
 
@@ -59,7 +83,7 @@ class Fir extends Thread {
 
     @Override
     public void run() {
-        System.out.println("Firul: " + getName() +
+        System.out.println("→ Firul pornit: " + getName() +
                 " | Grup: " + getThreadGroup().getName() +
                 " | Prioritate: " + getPriority());
         try {
